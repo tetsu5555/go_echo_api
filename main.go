6052a23d68067b75deeaaffe30ba40076c2890e6
tetsu5555 <tetete1118@gmail.com>
2019-02-16 16:22:18 +0900

    レスポンスヘッダーに書き込みを行うmiddlewareを作成。デフォルトのLoggerではなくカスタムLoggerを使うように設定

--- main.go
+++ main.go
@@ -17,13 +17,30 @@ func mainAdmin(c echo.Context) error {
 	return c.String(http.StatusOK, "horay you are on the secret admin page!")
 }
 
+// カスタムmiddllewareを作成
+// レスポンスヘッダーに書き込みを行うs
+func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderServer, "BlueBot/1.0")
+		c.Response().Header().Set("noHeader", "thisHaveNoMeaning")
+
+		return next(c)
+	}
+}
+
 func main() {
 	// Echoのインスタンス作る
 	e := echo.New()
 
+	e.Use(ServerHeader)
+
 	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
-	e.Use(middleware.Logger())
+	// e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	// Groupに対してmiddleware設定する
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
+	}))
 
 	// 静的ファイルのパスを設定
 	e.Static("/static", "static")
@@ -40,10 +57,7 @@ func main() {
 
 	// Groupを作成
 	g := e.Group("/admin")
-	// Groupに対してmiddleware設定する
-	g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
-	}))
+
 	// /admin/maiにリクエストした際に、mainAdminが呼び出されるようになる
 	g.GET("/main", mainAdmin)
 
